4c1e542ebd9cfe38164f6adc3cbb153d789e9f07
Tom <tom@inferis.org>
2024-06-29 11:16:50 +0200

    Mod menu serialization.

---
+++ src/main/java/org/inferis/lifeline/config/LifeLineModMenuIntegration.java
@@ -0,0 +1,41 @@
+package org.inferis.lifeline.config;
+
+import org.inferis.lifeline.LifeLine;
+import org.inferis.lifeline.config.LifeLineConfig.DisplayMode;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class LifeLineModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> configScreen(parent);
+    }
+
+    public Screen configScreen(Screen parentScreen) {
+        var config = LifeLine.CONFIG;
+
+        var builder = ConfigBuilder.create()
+            .setParentScreen(parentScreen)
+            .setTitle(Text.translatable("lifeline.config.title"));
+
+        var entryBuilder = builder.entryBuilder();
+
+        var category = builder.getOrCreateCategory(Text.translatable("lifeline.config.common"));
+        category
+            .addEntry(entryBuilder.startBooleanToggle(Text.translatable("lifeline.config.rendering_enabled"), config.renderingEnabled)
+                .setDefaultValue(true)
+                .setSaveConsumer(value -> { config.renderingEnabled = value; config.save(); })
+                .build())
+            .addEntry(entryBuilder.startEnumSelector(Text.translatable("lifeline.config.display_mode"), DisplayMode.class, config.displayMode)
+                .setDefaultValue(DisplayMode.HEARTS)
+                .setSaveConsumer(value -> { config.displayMode = value; config.save(); })
+                .build());
+
+        return builder.build();
+    }
+}
